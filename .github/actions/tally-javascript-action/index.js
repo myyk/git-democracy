78dd0df48f3bd79963b4b3a3e3b126cf71c40786
Myyk Seok <myyk.seok@gmail.com>
2020-08-20 19:23:09 +0800

    Create tallying action

---
+++ .github/actions/tally-javascript-action/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `comment-id` input defined in action metadata file
+  const commentId = core.getInput('comment-id');
+  console.log(`Voting Comment ${commentId}`);
+
+  core.setOutput("for", 123);
+  core.setOutput("against", 456);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
