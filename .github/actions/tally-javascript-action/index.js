e4243c1b7b13429c13d5eeaf210feb3c5eca148e
Myyk Seok <myyk.seok@gmail.com>
2020-08-21 18:19:28 +0800

    Add some behavior to tally

--- .github/actions/tally-javascript-action/index.js
+++ .github/actions/tally-javascript-action/index.js
@@ -1,17 +1,76 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `comment-id` input defined in action metadata file
-  const commentId = core.getInput('comment-id');
-  console.log(`Voting Comment ${commentId}`);
+const forIt = "+1";
+const againstIt = "-1";
 
-  core.setOutput("for", 123);
-  core.setOutput("against", 456);
+async function readReactionsCounts(octokit, repo, commentId, reactions) {
+  let responses = await Promise.allSettled(
+    reactions.map(async (reaction) => {
+      await octokit.reactions.listForPullRequestReviewComment({
+        owner: repo[0],
+        repo: repo[1],
+        comment_id: comment_id,
+        content: reaction,
+      });
+      // TODO: will need to follow get later pages if there are a lot of reactions
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+      core.info(`Getting '${reaction}' reactions.`);
+    })
+  );
+
+  let results = new Map();
+  for (let i = 0, l = responses.length; i < l; i++) {
+    lex reaction = reactions[i]
+    if (responses[i].status === "fulfilled") {
+      core.info(
+        `Reading reactions '${reaction}' from comment id '${comment_id}'.`
+      );
+      core.info(
+        `Response: '${responses[i]}'`
+      );
+      map.set(reaction, responses[i].data.length);
+    } else if (responses[i].status === "rejected") {
+      core.info(
+        `Reading reactions '${reaction}' from comment id '${comment_id}' failed with ${results[i].reason}.`
+      );
+      map.set(reaction, 0);
+    }
+  }
+
+  return results;
 }
+
+async function run() {
+  try {
+    // `comment-id` input defined in action metadata file
+    const inputs = {
+      token: core.getInput("token"),
+      repository: core.getInput("repository"),
+      commentId: core.getInput("comment-id"),
+    };
+    core.debug(`Inputs: ${inspect(inputs)}`);
+
+    const repository = inputs.repository
+      ? inputs.repository
+      : process.env.GITHUB_REPOSITORY;
+    const repo = repository.split("/");
+    core.debug(`repository: ${repository}`);
+
+    const octokit = github.getOctokit(inputs.token);
+
+    let reactionsToCount = new Set([forIt, againstIt]);
+    const reactionCounts = readReactionsCounts(octokit, repo, inputs.commentId, reactionsToCount)
+
+    core.setOutput("for", reactionCounts.get(forIt));
+    core.setOutput("against", reactionCounts.get(againstIt));
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
