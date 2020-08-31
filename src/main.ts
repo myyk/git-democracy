c10794dcfa18d6d7c8787313d28673faf310b25a
Myyk Seok <myyk.seok@gmail.com>
2020-08-31 15:43:36 +0800

    move reaction into typescript to inspect the response

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,46 @@
+import {inspect} from "util";
 import * as core from '@actions/core'
+import {github} from '@actions/github'
 import {wait} from './wait'
+import {readReactionsCounts} from './reactions'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const inputs = {
+      token: core.getInput("token"),
+      repository: core.getInput("repository"),
+      commentId: +(core.getInput("comment-id") as string),
+    };
+    core.debug(`Inputs: ${inspect(inputs)}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repository = inputs.repository
+      ? inputs.repository
+      : process.env.GITHUB_REPOSITORY as string;
+    const [owner, repo] = repository.split("/");
+    core.debug(`repository: ${owner}/${repo}`);
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(
+      inputs.token,
+      {
+        previews: ['squirrel-girl'],
+      },
+    );
+
+    const reactionCountsPromise = readReactionsCounts(octokit, owner, repo, inputs.commentId)
+    // const votingConfigPromise = readVotingConfig()
+
+    const reactionCounts = await reactionCountsPromise
+    // const votingConfig = await votingConfigPromise // TODO: add voting config to action outpus
+
+    // core.setOutput("for", reactionCounts[forIt]);
+    // core.setOutput("against", reactionCounts[againstIt]);
+    core.setOutput("for", 123);
+    core.setOutput("against", 456);
+
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message)
   }
