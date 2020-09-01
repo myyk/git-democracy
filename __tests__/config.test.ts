fe64ccfc1e57d5aa826ee8eb03f0721b31954338
Myyk Seok <myyk.seok@gmail.com>
2020-09-01 17:03:55 +0800

    fix some linter issues

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -3,35 +3,61 @@ import {readVotingConfig, Config as VotingConfig} from '../src/config'
 test('readVotingConfig can read a valid config', async () => {
   await expect(
     readVotingConfig('./__tests__/data/voting-valid.yml')
-  ).resolves.toEqual(new VotingConfig({percentageToApprove: 75, minVotersRequired: 10, minVotingWindowMinutes: 3600}));
+  ).resolves.toEqual(
+    new VotingConfig({
+      percentageToApprove: 75,
+      minVotersRequired: 10,
+      minVotingWindowMinutes: 3600
+    })
+  )
 })
 
 test('readVotingConfig has error on empty config', async () => {
   await expect(
     readVotingConfig('./__tests__/data/voting-empty.yml')
-  ).rejects.toThrowError('config data is not object type');
+  ).rejects.toThrowError('config data is not object type')
 })
 
 test('readVotingConfig has no error on missing percentageToApprove', async () => {
   await expect(
     readVotingConfig('./__tests__/data/voting-missing-percentageToApprove.yml')
-  ).resolves.toEqual(new VotingConfig({percentageToApprove: 0, minVotersRequired: 10, minVotingWindowMinutes: 3600}));
+  ).resolves.toEqual(
+    new VotingConfig({
+      percentageToApprove: 0,
+      minVotersRequired: 10,
+      minVotingWindowMinutes: 3600
+    })
+  )
 })
 
 test('readVotingConfig has error on invalid percentageToApprove', async () => {
   await expect(
     readVotingConfig('./__tests__/data/voting-invalid-percentageToApprove.yml')
-  ).rejects.toThrowError();
+  ).rejects.toThrowError()
 })
 
 test('readVotingConfig has error on invalid minVotersRequired', async () => {
   await expect(
     readVotingConfig('./__tests__/data/voting-invalid-minVotersRequired.yml')
-  ).resolves.toEqual(new VotingConfig({percentageToApprove: 75, minVotersRequired: 0, minVotingWindowMinutes: 3600}));
+  ).resolves.toEqual(
+    new VotingConfig({
+      percentageToApprove: 75,
+      minVotersRequired: 0,
+      minVotingWindowMinutes: 3600
+    })
+  )
 })
 
 test('readVotingConfig has error on invalid minVotingWindowMinutes', async () => {
   await expect(
-    readVotingConfig('./__tests__/data/voting-invalid-minVotingWindowMinutes.yml')
-  ).resolves.toEqual(new VotingConfig({percentageToApprove: 75, minVotersRequired: 10, minVotingWindowMinutes: 0}));
+    readVotingConfig(
+      './__tests__/data/voting-invalid-minVotingWindowMinutes.yml'
+    )
+  ).resolves.toEqual(
+    new VotingConfig({
+      percentageToApprove: 75,
+      minVotersRequired: 10,
+      minVotingWindowMinutes: 0
+    })
+  )
 })
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import {inspect} from 'util'
-import {promises as fsPromises, PathLike} from 'fs';
-import {safeLoad as yamlSafeLoad} from 'js-yaml';
+import {promises as fsPromises, PathLike} from 'fs'
+import {safeLoad as yamlSafeLoad} from 'js-yaml'
 // import {PathLike, FileHandle} from 'fs';
 
 // export class Config {
@@ -15,50 +15,61 @@ import {safeLoad as yamlSafeLoad} from 'js-yaml';
 //   }
 // }
 export class Config {
-  percentageToApprove: number;
-  minVotersRequired: number;
-  minVotingWindowMinutes: number;
+  percentageToApprove: number
+  minVotersRequired: number
+  minVotingWindowMinutes: number
 
-  constructor({percentageToApprove = 0, minVotersRequired = 0, minVotingWindowMinutes = 0}: {percentageToApprove?: number, minVotersRequired?: number , minVotingWindowMinutes?: number}) {
-    this.percentageToApprove = percentageToApprove;
-    this.minVotersRequired = minVotersRequired;
+  constructor({
+    percentageToApprove = 0,
+    minVotersRequired = 0,
+    minVotingWindowMinutes = 0
+  }: {
+    percentageToApprove?: number
+    minVotersRequired?: number
+    minVotingWindowMinutes?: number
+  }) {
+    this.percentageToApprove = percentageToApprove
+    this.minVotersRequired = minVotersRequired
     this.minVotingWindowMinutes = minVotingWindowMinutes
   }
 }
 
-export async function readVotingConfig(path: PathLike): Promise<Config>  {
+export async function readVotingConfig(path: PathLike): Promise<Config> {
   // read voting config
   return fsPromises
     .readFile(path, 'utf8')
-    .then((fileContents) => {
-      return yamlSafeLoad(fileContents);
-    }).then((configData) => {
+    .then(fileContents => {
+      return yamlSafeLoad(fileContents)
+    })
+    .then(configData => {
       // validate and sanitize values
-      console.log(`voting config: ${inspect(configData)}`);
+      console.log(`voting config: ${inspect(configData)}`)
 
       if (!(configData instanceof Object)) {
         throw new Error(`config data is not object type`)
       }
 
       const config = new Config(configData)
-      if(!(config instanceof Config)) {
-        throw new Error('voting yaml is not instance of Config');
+      if (!(config instanceof Config)) {
+        throw new Error('voting yaml is not instance of Config')
       }
 
       {
-        let percentageToApprove = config.percentageToApprove;
+        const percentageToApprove = config.percentageToApprove
         if (percentageToApprove < 0 || percentageToApprove > 100) {
-          throw new Error(`percentageToApprove=${percentageToApprove} but should be between 0 and 100 inclusively.`);
+          throw new Error(
+            `percentageToApprove=${percentageToApprove} but should be between 0 and 100 inclusively.`
+          )
         }
       }
 
       if (config.minVotersRequired < 0) {
-        config.minVotersRequired = 0;
+        config.minVotersRequired = 0
       }
       if (config.minVotingWindowMinutes < 0) {
-        config.minVotingWindowMinutes = 0;
+        config.minVotingWindowMinutes = 0
       }
 
       return config
-    });
+    })
 }
